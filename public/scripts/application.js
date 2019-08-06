56d9023730d07728646ba803143fb61bae661415
Travis Lawrence <travis.lawrence@uber.com>
2019-08-06 13:08:47 -0700

    Why do I bother?

--- public/scripts/application.js
+++ public/scripts/application.js
@@ -1,118 +1,145 @@
-'use strict';
+"use strict";
 
 const monthNames = [
-"January", "February", "March", "April", "May", "June", "July",
-"August", "September", "October", "November", "December"
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
 ];
 
-const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const dayNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
 
-function DateTile(date) {
-    this.date  = date;
-    this.day   = date.getDate();
-    this.dow   = date.getDay();
+class DateTile {
+  constructor(date) {
+    this.day = date.getDate();
+    this.dow = date.getDay();
     this.month = date.getMonth();
-    this.year  = date.getFullYear();
-}
+    this.year = date.getFullYear();
+  }
 
-DateTile.prototype = {
+  get monthName() {
+    return monthNames[this.month];
+  }
 
-    get monthName () {
-        return monthNames[this.month];
-    },
+  dateString = delimiter => {
+    delimiter = delimiter || "/";
+    return [this.month + 1, this.day, this.year].join(delimiter);
+  };
 
-    dateString: function (delimiter) {
-        delimiter = delimiter || '/';
-        return [this.month + 1, this.day, this.year].join(delimiter);
-    },
-
-    buildTile: function () {
-        return '<li class="week--tile"><div class="week--tile-day">' + this.day + '</div></li>'
-    }
+  buildTile = () =>
+    `<li class="week--tile"><div class="week--tile-day">${this.day}</div></li>`;
 }
 
 function chunkWeeks(dates) {
-    let weeks = [];
-    let weekCount = Math.ceil(dates.length / 7);
-    for (var i = 0; i < weekCount; i++) {
-        let weekIdx = i * 7;
-        let days = dates.slice(weekIdx, weekIdx + 7);
-        weeks.push(days);
-    }
-    return weeks;
+  let weeks = [];
+  let weekCount = Math.ceil(dates.length / 7);
+  for (var i = 0; i < weekCount; i++) {
+    let weekIdx = i * 7;
+    let days = dates.slice(weekIdx, weekIdx + 7);
+    weeks.push(days);
+  }
+  return weeks;
 }
 
-function buildWeeks (dates) {
-    return chunkWeeks(dates).reduce((pMonth, cMonth, i) => {
-        let weekHtml = cMonth.reduce((pDate, cDate) => {
-            return pDate += cDate === null ? '<li class="week--tile none"></li>' : cDate.buildTile();
-        }, '')
+function buildWeeks(dates) {
+  return chunkWeeks(dates)
+    .reduce((pMonth, cMonth, i) => {
+      let weekHtml = cMonth.reduce((pDate, cDate) => {
+        return (pDate +=
+          cDate === null
+            ? '<li class="week--tile none"></li>'
+            : cDate.buildTile());
+      }, "");
 
-        pMonth.push('<ul class="week week-' + i + '">' + weekHtml + '</ul>');
-        return pMonth;
-    }, []).join('');    
+      pMonth.push(`<ul class="week week-${i}">${weekHtml}</ul>`);
+      return pMonth;
+    }, [])
+    .join("");
 }
 
-function buildMonth (dates) {
-    let firstDay  = dates[0];
-    if (firstDay.dow > 0) {
-        let weekPad = new Array(firstDay.dow).fill(null);
-        dates = weekPad.concat(dates);
-    }
+function buildMonth(dates) {
+  let firstDay = dates[0];
+  if (firstDay.dow > 0) {
+    let weekPad = new Array(firstDay.dow).fill(null);
+    dates = weekPad.concat(dates);
+  }
 
-    let header = dayNames.map((day) => {
-        return '<div class="month--header-day">' + day + '</div>';
-    }).join('');
+  let header = dayNames
+    .map(day => {
+      return `<div class="month--header-day">${day}</div>`;
+    })
+    .join("");
 
-    return  '<div class="month ' + firstDay.monthName.toLowerCase() + '">' +
-                '<div class="month--name">' + firstDay.monthName + ' ' + firstDay.year + '</div>' +
-                '<div class="month--header">' + header + '</div>' +
-                buildWeeks(dates) +
-            '</div>';
+  return `
+    <div class="month ${firstDay.monthName.toLowerCase()}">
+        <div class="month--name">${firstDay.monthName} ${firstDay.year}</div>
+        <div class="month--header">${header}</div>
+        ${buildWeeks(dates)}
+    </div>`;
 }
 
 function getDateRange() {
-    let start = new Date(startDateEl.value),
-        end   = new Date(endDateEl.value),
-        countDays = 0;
+  const start = new Date(startDateEl.value);
+  const end = new Date(endDateEl.value);
+  let countDays = 0;
 
-    let daysOfYear = {};
-    for (let date = start; date <= end; date.setDate(date.getDate() + 1)) {
-        let tile = new DateTile(new Date(date));
+  const daysOfYear = {};
+  for (let date = start; date <= end; date.setDate(date.getDate() + 1)) {
+    let tile = new DateTile(new Date(date));
 
-        if (!daysOfYear.hasOwnProperty(tile.year)) {
-            daysOfYear[tile.year] = {};
-        }
-
-        if (!daysOfYear[tile.year].hasOwnProperty(tile.month)) {
-            daysOfYear[tile.year][tile.month] = [];
-        }
-
-        daysOfYear[tile.year][tile.month].push(tile);
-        countDays++;
+    if (!daysOfYear.hasOwnProperty(tile.year)) {
+      daysOfYear[tile.year] = {};
     }
 
-    let output = Object.keys(daysOfYear).reduce((output, year) => {
-        for (let j in daysOfYear[year]) {
-            let month = daysOfYear[year][j];
-            output += buildMonth(month);
-        }
-        return output;
-    }, '');
+    if (!daysOfYear[tile.year].hasOwnProperty(tile.month)) {
+      daysOfYear[tile.year][tile.month] = [];
+    }
 
-    countEl.textContent = 'Day Count: ' + countDays;
-    outputEl.innerHTML = output;
+    daysOfYear[tile.year][tile.month].push(tile);
+    countDays++;
+  }
+
+  let output = Object.keys(daysOfYear).reduce((output, year) => {
+    for (let j in daysOfYear[year]) {
+      let month = daysOfYear[year][j];
+      output += buildMonth(month);
+    }
+    return output;
+  }, "");
+
+  countEl.textContent = "Day Count: " + countDays;
+  outputEl.innerHTML = output;
 }
 
-let startDateEl  = document.getElementById('start'),
-    endDateEl    = document.getElementById('end'),
-    dateSubmit   = document.getElementById('submit-dates'),
-    countEl      = document.getElementById('date-count'),
-    outputEl     = document.getElementById('json-dates'),
-    defaultEnd   = new Date(),
-    defaultStart = new Date(defaultEnd.getFullYear() - 1, defaultEnd.getMonth(), '1');
+let startDateEl = document.getElementById("start"),
+  endDateEl = document.getElementById("end"),
+  dateSubmit = document.getElementById("submit-dates"),
+  countEl = document.getElementById("date-count"),
+  outputEl = document.getElementById("json-dates"),
+  defaultEnd = new Date(),
+  defaultStart = new Date(
+    defaultEnd.getFullYear() - 1,
+    defaultEnd.getMonth(),
+    "1"
+  );
 
 startDateEl.value = new DateTile(defaultStart).dateString();
-endDateEl.value   = new DateTile(defaultEnd).dateString();
-dateSubmit.addEventListener('click', getDateRange);
+endDateEl.value = new DateTile(defaultEnd).dateString();
+dateSubmit.addEventListener("click", getDateRange);
 getDateRange();
