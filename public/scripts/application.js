086970ad1a42f8babc0e3638a8da55a24faf4d63
Travis Lawrence <travis.lawrence@uber.com>
2019-12-13 23:54:39 -0800

    Ok, stop finally

--- public/scripts/application.js
+++ public/scripts/application.js
@@ -29,6 +29,13 @@ const WEEK_DAY_ELEMENTS = DAY_NAMES.map(
   day => `<div class="month--header-day">${day}</div>`
 ).join("");
 
+function formatDate(date = new Date()) {
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
+  return dateString({ month, year, day });
+}
+
 function zeroPad(num) {
   return `0${num}`.slice(-2);
 }
@@ -37,84 +44,40 @@ function dateString({ month, day, year }) {
   return [year, zeroPad(month + 1), zeroPad(day)].join("-");
 }
 
-function dateTile(date) {
-  return {
-    day: date.getDate(),
-    dow: date.getDay(),
-    month: date.getMonth(),
-    year: date.getFullYear(),
-    monthName: MONTH_NAMES[date.getMonth()]
-  };
-}
-
-function buildMonthGrid(dates) {
-  return dates
-    .map(date =>
-      date === null
-        ? '<div class="week--tile none"></div>'
-        : `<div class="week--tile">${date.day}</div>`
-    )
-    .join("");
-}
-
-function buildMonth(dates) {
-  const firstDay = dates[0];
-  if (firstDay.dow > 0) {
-    const weekPad = new Array(firstDay.dow).fill(null);
-    dates = weekPad.concat(dates);
+class Month {
+  constructor(month, year) {
+    const date = new Date(year, month);
+    this.dow = date.getDay();
+    this.month = month;
+    this.year = year;
+    this.dayCount = new Date(year, month + 1, 0).getDate();
+    this.monthName = MONTH_NAMES[date.getMonth()];
   }
 
-  return `
-    <div class="month ${firstDay.monthName.toLowerCase()}" id="${firstDay.monthName.toLowerCase()}-${
-    firstDay.year
-  }">
-        <div class="month--name">${firstDay.monthName} ${firstDay.year}</div>
-        <div class="month--header">${WEEK_DAY_ELEMENTS}</div>
-        <div class="month--grid">${buildMonthGrid(dates)}</div>
+  render = (startDate, endDate) => {
+    let days = [];
+    for (let i = 1; i <= this.dayCount; i++) {
+      const isOutofRange = i < startDate || i > endDate;
+      days.push(
+        `<div class="week--tile${isOutofRange ? " fade" : ""}">${i}</div>`
+      );
+    }
+    if (this.dow > 0) {
+      const weekPad = new Array(this.dow).fill(
+        '<div class="week--tile none"></div>'
+      );
+      days = weekPad.concat(days);
+    }
+
+    return `
+    <div class="month ${this.monthName.toLowerCase()}" id="${this.monthName.toLowerCase()}-${
+      this.year
+    }">
+    <div class="month--name">${this.monthName} ${this.year}</div>
+    <div class="month--header">${WEEK_DAY_ELEMENTS}</div>
+    <div class="month--grid">${days.join("")}</div>
     </div>`;
-}
-
-function getDateRange() {
-  const start = new Date(startEl.value.split("-"));
-  const end = new Date(endEl.value.split("-"));
-  let countDays = 0;
-
-  const daysOfYear = {};
-  for (let date = start; date <= end; date.setDate(date.getDate() + 1)) {
-    const tile = dateTile(date);
-
-    if (!daysOfYear.hasOwnProperty(tile.year)) {
-      daysOfYear[tile.year] = {};
-    }
-
-    if (!daysOfYear[tile.year].hasOwnProperty(tile.month)) {
-      daysOfYear[tile.year][tile.month] = [];
-    }
-
-    daysOfYear[tile.year][tile.month].push(tile);
-    countDays++;
-  }
-
-  const output = {
-    grid: "",
-    list: ""
   };
-
-  Object.keys(daysOfYear).reduce((el, year) => {
-    for (let j in daysOfYear[year]) {
-      let month = daysOfYear[year][j];
-      el.grid += buildMonth(month);
-      const monthName = MONTH_NAMES[j];
-      el.list += `<div class='${monthName.toLowerCase()} list'>
-        <a href='#${monthName.toLowerCase()}-${year}'>${monthName} ${year}</a>
-      </div>`;
-    }
-    return el;
-  }, output);
-
-  countEl.textContent = `Day Count: ${countDays}`;
-  dateListEl.innerHTML = output.list;
-  calendarEl.innerHTML = output.grid;
 }
 
 const dateSubmit = document.getElementById("dates-form");
@@ -127,8 +90,8 @@ const calendarEl = document.getElementById("json-dates");
 const defaultEnd = new Date();
 const defaultStart = new Date(defaultEnd.getFullYear(), 0);
 
-startEl.value = dateString(dateTile(defaultStart));
-endEl.value = dateString(dateTile(defaultEnd));
+startEl.value = formatDate(defaultStart);
+endEl.value = formatDate(defaultEnd);
 
 dateSubmit.addEventListener("submit", e => {
   e.preventDefault();
@@ -136,3 +99,61 @@ dateSubmit.addEventListener("submit", e => {
 });
 
 getDateRange();
+
+function dayRange(start, end, month) {
+  const range = { start: 1, end: month.dayCount };
+  if (month.year === start.getFullYear() && month.month === start.getMonth()) {
+    range.start = start.getDate();
+  }
+  if (month.year === end.getFullYear() && month.month === end.getMonth()) {
+    range.end = end.getDate();
+  }
+  return range;
+}
+
+function getDateRange() {
+  const start = new Date(startEl.value.split("-"));
+  const end = new Date(endEl.value.split("-"));
+
+  const months = [];
+  for (let year = start.getFullYear(); year <= end.getFullYear(); year++) {
+    for (let month = 0; month < 12; month++) {
+      if (year >= end.getFullYear() && month > end.getMonth()) {
+        break;
+      }
+      const tile = new Month(month, year);
+      months.push(tile);
+    }
+  }
+
+  renderCalendar(months);
+}
+
+function renderCalendar(months) {
+  const start = new Date(startEl.value.split("-"));
+  const end = new Date(endEl.value.split("-"));
+  let countDays = 0;
+  const output = {
+    grid: "",
+    list: ""
+  };
+
+  let finishDay = 0;
+  months.reduce((el, month) => {
+    const { start: startDay, end: endDay } = dayRange(start, end, month);
+    el.grid += month.render(startDay, endDay);
+    const { year, monthName } = month;
+    el.list += `<div class='${monthName.toLowerCase()} list'>
+    <a href='#${monthName.toLowerCase()}-${year}'>${monthName} ${year}</a>
+    </div>`;
+
+    countDays += month.dayCount;
+    finishDay = month.dayCount - endDay;
+    return el;
+  }, output);
+
+  countEl.textContent = `Day Count: ${countDays -
+    (start.getDate() - 1 + finishDay)}`;
+  dateListEl.innerHTML = output.list;
+  calendarEl.innerHTML = output.grid;
+}
